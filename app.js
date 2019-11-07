67fe7d64fce08ec0baacf268d0b2e86290cae371
Poniks <nadstogakuba@gmail.com>
2019-11-07 17:18:21 +0100

    Update js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const btn = document.querySelector('div');
 
 //javascript, jQuery
 let idGif;
-var xhr = $.get("http://api.giphy.com/v1/gifs/random?tag=cat&api_key=dc6zaTOxFJmzC");
+var xhr = $.get("https://api.giphy.com/v1/gifs/random?tag=cat&api_key=dc6zaTOxFJmzC");
 
 xhr.done(function(data) {
   console.log("success got data", data);
@@ -13,7 +13,7 @@ xhr.done(function(data) {
 });
 
 btn.addEventListener('click', () => {
-  xhr = $.get("http://api.giphy.com/v1/gifs/random?tag=cat&api_key=dc6zaTOxFJmzC");
+  xhr = $.get("https://api.giphy.com/v1/gifs/random?tag=cat&api_key=dc6zaTOxFJmzC");
 
   xhr.done(function(data) {
     console.log("success got data", data);
